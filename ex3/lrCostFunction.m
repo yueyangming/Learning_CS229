1ef626b813e96893e9c30e17e1ef06073c0921ac
Harold_Yue <yueyangming1024@sina.com>
2016-09-07 20:44:54 +0800

    Finish IrCostFunction

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,14 +36,18 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
 
+J = -sum( y.*log(h) + (1-y) .* log(1-h) ) / m;
 
+grad =  X' * (h-y) / m;
 
+temp = theta;
+temp(1,:) = 0;
 
+J = J + sum(temp.^2) * lambda / 2 / m;
 
-
-
-
+grad = grad + temp * lambda / m; 
 
 % =============================================================
 
